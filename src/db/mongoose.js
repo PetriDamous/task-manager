2af6c7b4b34f7ff9b0e48f94a9f9c47146681d61
dwilliams64 <dwilliams_64@yahoo.com>
2021-03-09 20:48:11 -0600

    Created models, created creation routes, created read routes

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,4 @@
 const mongoose = require('mongoose');
-const User = require('./models/users');
-const Task = require('./models/tasks');
-
 
 const dbUrl = 'mongodb://127.0.0.1:27017/task-manger-api';
 
@@ -9,27 +6,3 @@ mongoose.connect(dbUrl, {
     useNewUrlParser: true,
     useCreateIndex: true
 });
-
-
-
-// const user1 = new User({
-//     name: 'Red Riding Hood',
-//     email: ' redHood@gmail.com ',
-//     password: 'P@sSWoRd#69!',
-//     age: 18,
-    
-// });
-
-// user1.save()
-//     .then((user1) => console.log(user1))
-//     .catch((e) => console.log('Error:', e));
-
-
-
-const task1 = new Task({
-    completed: true
-});
-
-task1.save()
-    .then(console.log(task1))
-    .catch((e) => console.log(e));
